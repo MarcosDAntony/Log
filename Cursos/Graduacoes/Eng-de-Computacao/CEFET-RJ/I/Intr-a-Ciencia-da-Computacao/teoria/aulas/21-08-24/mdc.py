466494cb48ce8bd1c2a8f068db5e9b7f0f48a8c1
MarcosAntony7 <marcosantony.asp@gmail.com>
2024-08-21 12:05:44 -0300

    21-08-24

--- Cursos/Graduacoes/Eng-de-Computacao/CEFET-RJ/I/Intr-a-Ciencia-da-Computacao/teoria/aulas/21-08-24/mdc.py
+++ Cursos/Graduacoes/Eng-de-Computacao/CEFET-RJ/I/Intr-a-Ciencia-da-Computacao/teoria/aulas/21-08-24/mdc.py
@@ -1,1 +1,13 @@
 # Crie um algoritmo que cálcule o MDC de dois números de forma sequêncial
+def mdc(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+# Exemplo de uso
+a = int(input("Iº Dividendo: "))
+b = int(input("IIº Dividendo: "))
+
+resultmdc = mdc(a, b)
+print(f"O Máximo Divisor Comum de {a} e {b} é {resultmdc}")
+2
--- Cursos/Graduacoes/Eng-de-Computacao/CEFET-RJ/I/Intr-a-Ciencia-da-Computacao/teoria/aulas/21-08-24/mdc2.py
+++ Cursos/Graduacoes/Eng-de-Computacao/CEFET-RJ/I/Intr-a-Ciencia-da-Computacao/teoria/aulas/21-08-24/mdc2.py
@@ -1,2 +1,18 @@
 # Crie um algoritmo que de o resultado do MDC de dois ou mais números e de forma recursiva
 
+def mdc(a, b):
+    """
+    Calcula o Máximo Divisor Comum (MDC) de dois números inteiros a e b
+    utilizando o algoritmo de Euclides de forma recursiva.
+    """
+    if b == 0:
+        return a
+    else:
+        return mdc(b, a % b)
+
+# Exemplo de uso
+a = int(input("Digite o primeiro número: "))
+b = int(input("Digite o segundo número: "))
+
+resultmdc = mdc(a, b)
+print(f"O MDC de {a} e {b} é {resultmdc}.")
